d7bbf41bcefb8b3b88756e485a66d2811f3a85a6
gintrator <g.intrator@gmail.com>
2015-02-23 17:04:15 -0800

    Makefile and raytracer.cpp

---
+++ Makefile
@@ -0,0 +1,5 @@
+CC = g++
+RM = /bin/rm -f 
+
+all:
+	$(CC) raytracer.cpp -o raytracer
---
+++ raytracer.cpp
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+	printf("Ray Tracer!\n");
+	return 0;
+}
