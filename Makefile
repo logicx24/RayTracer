4a5b21fb4034f14f0a6e881a19b01a31ff716a42
gintrator <g.intrator@gmail.com>
2015-03-09 22:57:45 -0700

    basic stuff working, vec3 class, compiles, even

--- Makefile
+++ Makefile
@@ -1,12 +1,14 @@
 CC = g++
 RM = /bin/rm -f 
 
-all: main
+all: clean main 
 main: raytracer.o lodepng.o
 	$(CC) -o raytracer raytracer.o lodepng.o
 raytracer.o:
 	$(CC) -c raytracer.cpp -o raytracer.o
 lodepng.o:
 	$(CC) -c lodepng.cpp -o lodepng.o
+file:
+	./raytracer > ~/Desktop/out.txt
 clean:
-	$(RM) *.o raytracer lodepng
+	$(RM) *.o raytracer lodepng img.png
--- raytracer.cpp
+++ raytracer.cpp
@@ -1,127 +1,13 @@
 #include <stdio.h>
 #include <math.h>
 #include "lodepng.h"
+#include "algebra3.h"
 
 #include <vector>
 #include <iostream>
 
 
-#define MAT_ROTATE_X 101
-#define MAT_ROTATE_Y 102
-#define MAT_ROTATE_Z 103
-
 using namespace std;
-
-class Vector {
-public:
-	float x, y, z, magnitude;
-
-	Vector(float _x, float _y, float _z) {
-		x = _x;
-		y = _y;
-		z = _z;
-		magnitude = sqrt(x * x + y * y + z * z);
-	}
-
-	Vector* operator+(Vector *vec) {
-		return new Vector(x + vec->x, y + vec->y, z + vec->z);
-	}
-
-	Vector* operator-(Vector *vec) {
-		return new Vector(x - vec->x, y - vec->y, z - vec->z);
-	}
-
-	float operator*(Vector *vec) {
-		return x * vec->x + y * vec->y + z * vec->z;
-	}
-
-    float dotp(Vector *vec) {
-		return x * vec->x + y * vec->y + z * vec->z;
-	}
-
-	Vector* operator*(float scale) {
-		return new Vector(x * scale, y * scale, z * scale);
-	}
-
-	void normalize() {
-		x = x/magnitude;
-		y = y/magnitude;
-		z = z/magnitude;
-	}
-};
-
-class Point {
-public:
-	float x, y, z;
-
-	Point(float _x, float _y, float _z) {
-		x = _x;
-		y = _y;
-		z = _z;
-	}
-
-	Vector* operator-(Point *p) {
-		return new Vector(x - p->x, y - p->y, z - p->z);
-	}
-};
-
-class Ray {
-public:
-	Point *pos;
-	Vector *dir;
-	float t_min, t_max;
-	
-	Ray(Point *p, Vector *v) {
-		// Possibly make copies?
-		pos = p;
-		dir = v;
-		t_min = 1;
-		t_max = 10000000.0f;
-	}
-};
-
-class Matrix {
-public:
-	float mat[4][4];
-
-	Matrix(int kind, float theta) {
-		for (int x = 0; x < 4; x++) {
-			for (int y = 0; y < 4; y++) {
-				mat[y][x] = 0.0f;
-			}
-		}
-		switch (kind) {
-			case MAT_ROTATE_X:
-				mat[0][0] = 1.0f;
-				mat[1][1] = cos(theta);
-				mat[2][1] = -sin(theta);
-				mat[2][1] = sin(theta);
-				mat[2][2] = cos(theta);
-				mat[3][3] = 1.0f;
-				break;
-			case MAT_ROTATE_Y:
-				mat[0][0] = cos(theta);
-				mat[0][2] = -sin(theta);
-				mat[1][1] = 1.0f;
-				mat[2][0] = sin(theta);
-				mat[2][1] = cos(theta);
-				mat[3][3] = 1.0f;
-				break;
-			case MAT_ROTATE_Z:
-				mat[0][0] = cos(theta);
-				mat[0][2] = -sin(theta);
-				mat[1][0] = sin(theta);
-				mat[1][1] = cos(theta);
-				mat[2][2] = 1.0f;
-				mat[3][3] = 1.0f;
-				break;
-		}
-	}
-
-	Matrix(int kind) {
-
-	}
-};
 
 class Color {
 public:
@@ -133,10 +19,29 @@ public:
     }
 };
 
+
+class Ray {
+public:
+	vec3 pos;
+	vec3 vec;
+	float t_min, t_max;
+	
+	Ray(vec3 &p, vec3 &v) {
+		// Possibly make copies?
+		pos = p;
+		vec = v;
+		t_min = 1;
+		t_max = 10000000.0f;
+	}
+};
+
+
+
 class Sample {
 public:
     int x, y;
-    Sample(int _x, int _y) {
+
+    Sample(int _x = 0, int _y = 0) {
     	x = _x;
     	y = _y;
     }
@@ -155,7 +60,7 @@ public:
     }
 
     bool generateSample(Sample *sample) {
-        if (y  == height) {
+        if (y == height) {
         	return false;
         }
     	sample->x = x;
@@ -175,16 +80,19 @@ class Film {
 public:
     int width, height;
     Color *film;
+
+    Film(){}
+
     Film(int _width, int _height) {
     	width = _width;
     	height = _height;
     	film = new Color[width * height];
     }
 
-    void writeToFilm(Sample *sample, Color *color) {
-        film[sample->y * width + sample->x].r = color->r;
-        film[sample->y * width + sample->x].g = color->g;
-        film[sample->y * width + sample->x].b = color->b;
+    void writeToFilm(Sample &sample, Color &color) {
+        film[sample.y * width + sample.x].r = color.r;
+        film[sample.y * width + sample.x].g = color.g;
+        film[sample.y * width + sample.x].b = color.b;
     }
 
     void writeFile() {
@@ -215,112 +123,136 @@ public:
 
 class Sphere {
 public:
-	Point *center;
+	vec3 center;
 	float radius;
 
-	Sphere(Point *_center, float _radius) {
+	Sphere(vec3 &_center, float _radius) {
 		center = _center;
 		radius = _radius;
 	}
 
-	bool intersects(Ray *ray) {
+	bool intersects(Ray &ray) {
 		// (d * (e - c)) - (d * d)((e-c) * (e-c))
-		Vector *p_c = new Vector(ray->pos->x - center->x, ray->pos->y - center->y, ray->pos->z - center->z);
-        float discriminant = pow(ray->dir->dotp(p_c), 2) - 
-					 (ray->dir->dotp(ray->dir))*((p_c->dotp(p_c) - pow(radius, 2)));
-		return discriminant >= 0;
+		vec3 p_c = ray.pos - center;
+        //p_c.normalize();
+        float discriminant = pow(ray.vec * p_c, 2) - 
+        ((ray.vec * ray.vec) * ((p_c * p_c) - pow(radius, 2)));
+        cout << "d: " << discriminant << endl;
+		return discriminant >=  0;
 	}
-    /*
-	Point intersectsAt(Ray *ray) {
-		float discriminant = pow(ray->dir * (ray->pos - center), 2) - 
-					 (ray->dir * ray->dir)*(((ray->pos - center)*(ray->pos - center))
-					 - pow(radius, 2));
-		float soln1 = (((ray->dir) * -1.0f) * (ray->pos - center) + sqrt(discriminant))/(ray->dir * ray->dir);
-		float soln2 = (((ray->dir) * -1.0f)* (ray->pos - center) - sqrt(discriminant))/(ray->dir * ray->dir);
-		//Figure out which solution is correct.
-		return new Point(ray->pos->x + soln1*ray->dir->x, ray->pos->y + soln1*ray->dir->y, ray->pos->z + soln1*ray->dir->z);
-        
-	}*/
-
 };
 
-class Scene {
+class Camera {
 public:
-	Point *eye, *ul, *ur, *ll, *lr;
-	int width, height;
-	Film *film;
 
-	Scene(Point *_eye, Point *_ul, Point *_ur, Point *_ll, Point *_lr, 
-		  Film *_film, int _width, int _height) {
-		eye = _eye;
+	vec3 eye, ul, ur, ll, lr;
+    int width, height;
+
+	Camera(){}
+
+	Camera(vec3 &_eye, vec3 &_ul, vec3 &_ur, vec3 &_ll, vec3 &_lr, 
+	       int _width, int _height) {
+	    eye = _eye;
 		ul = _ul;
 		ur = _ur;
 		ll = _ll;
 		lr = _lr;
+		width = _width;
+		height = _height;
+	}
+
+	Ray generateRay(Sample &sample) {
+        float u = sample.x / float(width);
+        float v = sample.y / float(height);
+        // cout << sample.x << " : " << sample.y << endl;
+        // cout << width << " : " << height << endl;
+        // cout << u << " : " << v << endl;
+        vec3 c = (ll*(1-u) + lr*u)*(1-v) + (ul*(1-u) + ur*u)*v;
+        //cout << "1. " << c[VX] << " : " << c[VY] << " : " << c[VZ] << endl;
+        vec3 testRayVec = c - eye;
+        testRayVec = testRayVec.normalize();
+        //cout << "2. " << c[VX] << " : " << c[VY] << " : " << c[VZ] << endl;
+	    return Ray(eye, testRayVec); 
+	}
+};
+
+
+class Scene {
+public:
+	Camera cam;
+	Film film;
+	vector<Sphere> objects;
+	int width, height;
+	
+	Scene(Camera &_cam, Film &_film, int _width, int _height)  {
+		cam = _cam;
 		film = _film;
 		width = _width;
 		height = _height;
 	}
+
+	void addObject(Sphere &s) {
+ 		objects.push_back(s);
+	}
     
-    void testSphere() {
-    	Sampler *sampler = new Sampler(width, height);
-    	Sample *sample = new Sample(0, 0);
-    	Color *black = new Color(0, 0, 0);
-    	Color *red = new Color(255, 0, 0);
-    	Point *center = new Point(50, 50, 0.5);
-    	Sphere *sphere = new Sphere(center, 10);
-        while (sampler->generateSample(sample)) {
-        	Vector *testRayDir = new Vector(sample->x - eye->x, sample->y - eye->y, -eye->z);
-        	Point *testRayPoint = new Point(eye->x, eye->y, eye->z);
-        	Ray *testRay = new Ray(testRayPoint, testRayDir);
-            if (sphere->intersects(testRay)) {
-            	film->writeToFilm(sample, red);
-            	//cout << "RED" << endl;
-            } else {
-            	film->writeToFilm(sample, black);
-            	//cout << "BLACK" << endl;
-            }
-            delete testRayDir;
-            delete testRayPoint;
-            delete testRay;
-        }
-        film->writeFile();
-    }
 
     void renderLoop() {
-        
+    	Sampler sampler = Sampler(width, height);
+    	Sample sample = Sample();
+    	// Ray testRay = Ray(rayPos, rayVec);
+    	while (sampler.generateSample(&sample)) {
+    		//cout << "x: " << sample.x << " y: " << sample.y << endl;
+    		Ray testRay = cam.generateRay(sample);
+    		Color c = traceRay(testRay);
+    		//cout << "r: " << c.r << " g: " << c.g << " b: " << c.b << endl;
+    		film.writeToFilm(sample, c);
+    	}
+
+        film.writeFile();
+    }
+
+    Color traceRay(Ray &ray) {
+    	Color black = Color(0, 0, 0);
+    	Color red = Color(255, 0, 0);
+    	//cout << objects.size() << endl;
+		for (int i = 0; i < objects.size(); i++) {
+			if (objects[i].intersects(ray)) {
+				return Color(255, 0, 0);
+				// Recurse from objects[i]->intersectsAt(testRay) and bounce dir!
+				// Shade!
+
+			} else {
+				return Color(0, 0, 0);
+			}
+		}
+		return Color(0, 0, 0);
     }
 
 };
 
 
 
-void testFilm() {
-	int height = 100;
-	int width = 100;
-	Film *film = new Film(width, height);
-	Sampler *sampler = new Sampler(width, height);
-	Sample *sample = new Sample(0, 0);
-	Color *color = new Color(0, 0, 0);
-    while (sampler->generateSample(sample)) {
-    	//cout << "x: " << sample->x << endl;
-    	//cout << "y: " << sample->y << endl;
-        film->writeToFilm(sample, color);
-    }
-    film->writeFile();
-}
-
 
 int main(int argc, char* argv[]) {
-	printf("Ray Tracer1!\n");
-	Point *eye = new Point(50, 50, -10);
-	Point *ul = new Point (0, 99, 0);
-	Point *ur = new Point (99, 99, 0);
-	Point *ll = new Point (0, 0, 0);
-	Point *lr = new Point (99, 0, 0);
-	Film *film = new Film(100, 100);
-	Scene *scene = new Scene(eye, ul, ur, ll, lr, film, 100, 100);
-	scene->testSphere();
+	printf("Ray Tracer!\n");
+	int width = 100;
+	int height = 100;
+	vec3 eye = vec3(0, 0, 0);
+	vec3 ul = vec3(-1, 1, -1);
+	vec3 ur = vec3(1, 1, -1);
+	vec3 ll = vec3(-1, -1, -1);
+	vec3 lr = vec3(1, -1, -1);
+	Film film = Film(width, height);
+    Camera cam = Camera(eye, ul, ur, ll, lr, width, height);
+	Scene scene = Scene(cam, film, width, height);
+
+
+	vec3 sphereCenter = vec3(0, 0, -2);
+	Sphere sphere = Sphere(sphereCenter, 1);
+	scene.addObject(sphere);
+
+
+	scene.renderLoop();
 	return 0;
 }
 
